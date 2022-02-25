d63bc71cec1576f1866163b5805d6216a64f82db
sectionr0 <sectionr0@gmail.com>
2022-02-26 08:42:44 +0900

    setting navigation
    네비게이션 기능 추가

--- app/src/main/java/com/example/socialproject/fragments/FeedFragment.kt
+++ app/src/main/java/com/example/socialproject/fragments/FeedFragment.kt
@@ -5,56 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.socialproject.R
+import com.example.socialproject.databinding.FragmentFeedBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class FeedFragment : Fragment() {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [feedFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class feedFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding : FragmentFeedBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment feedFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            feedFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_feed, container, false)
+
+        binding.homeClubTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_feedFragment_to_homeFragment)
+        }
+
+        binding.socialTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_feedFragment_to_socialFragment)
+        }
+
+        binding.clubAddTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_feedFragment_to_clubFragment)
+        }
+
+        binding.profileTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_feedFragment_to_profileFragment)
+        }
+
+
+        return binding.root
     }
 }
--- app/src/main/java/com/example/socialproject/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/socialproject/fragments/HomeFragment.kt
@@ -5,9 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.socialproject.R
+import com.example.socialproject.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding : FragmentHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +23,26 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+
+        binding.socialTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_socialFragment)
+        }
+
+        binding.clubAddTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_clubFragment)
+        }
+
+        binding.feedTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_feedFragment)
+        }
+
+        binding.profileTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_profileFragment)
+        }
+
+        return binding.root
     }
 
 }
