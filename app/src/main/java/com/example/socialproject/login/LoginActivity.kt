ab86252c3e44248f6a5bffb7fc0fbf0f92cf5e78
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2022-02-26 23:29:03 +0900

    add kakao api gradle
    카카오 로그인 api 그래들 설정

--- app/src/main/java/com/example/socialproject/login/LoginActivity.kt
+++ app/src/main/java/com/example/socialproject/login/LoginActivity.kt
@@ -3,9 +3,11 @@ package com.example.socialproject.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import com.example.socialproject.HomeActivity
 import com.example.socialproject.R
+import com.kakao.sdk.common.util.Utility
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,9 @@ class LoginActivity : AppCompatActivity() {
         val loginbtn = findViewById<Button>(R.id.loginbtn)
         val kakaobtn = findViewById<Button>(R.id.kakaologinbtn)
         val googlebtn = findViewById<Button>(R.id.googleloginbtn)
+
+        val keyHash = Utility.getKeyHash(this)
+        Log.d("Hash", keyHash)
 
         loginbtn.setOnClickListener {
             val intent = Intent(this,HomeActivity::class.java)
