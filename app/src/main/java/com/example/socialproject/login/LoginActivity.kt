f6a5876c05ff98a213791a75a77e77b5c24b43f0
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2022-02-26 13:44:40 +0900

    add splash
    임시 스플래쉬 뷰 입니다

---
+++ app/src/main/java/com/example/socialproject/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.socialproject.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.socialproject.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
