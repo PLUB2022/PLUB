14903745e5d7b17347daba15ce7cc560c3f15688
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2022-03-03 01:42:02 +0900

    임시 viewpager
    임시 뷰페이저

--- app/src/main/java/com/example/socialproject/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/socialproject/fragments/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.example.socialproject.fragments
 
+import android.graphics.Color.red
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +10,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.viewpager2.widget.ViewPager2
 import com.example.socialproject.R
 import com.example.socialproject.databinding.FragmentHomeBinding
 
@@ -25,6 +28,29 @@ class HomeFragment : Fragment() {
     ): View? {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+
+        var bgColors = arrayListOf<Int>(
+            R.color.black,
+            R.color.white,
+            R.color.purple_200,
+            R.color.lightgray,
+            R.color.teal_200
+        )
+
+        // RecyclerView.Adapter<ViewHolder>()
+        binding.titleViewPager.adapter = PagerRecyclerAdapter(bgColors)
+        // ViewPager의 Paging 방향은 Horizontal
+        binding.titleViewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+        binding.titleViewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+
+            // Paging 완료되면 호출
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                Log.d("ViewPagerFragment", "Page ${position+1}")
+            }
+        })
+
 
         binding.socialTap.setOnClickListener {
             it.findNavController().navigate(R.id.action_homeFragment_to_socialFragment)
