515c7ed4b1737ca95a747d0584600c5d2fa057aa
sectionr0 <sectionr0@gmail.com>
2022-03-09 03:13:40 +0900

    merge tablayout2

--- app/src/main/java/com/example/socialproject/mainFragments/HomeFragment.kt
+++ app/src/main/java/com/example/socialproject/mainFragments/HomeFragment.kt
@@ -1,40 +1,39 @@
 package com.example.socialproject.mainFragments
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
-import android.view.*
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.appcompat.widget.Toolbar
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.setupWithNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.example.socialproject.R
 import com.example.socialproject.databinding.FragmentHomeBinding
 import com.example.socialproject.homeFragment.PostFragmentStateAdapter
 import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.tabs.TabItem
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
-import java.lang.Math.abs
 
 class HomeFragment : Fragment() {
 
-    private lateinit var binding: FragmentHomeBinding
+    private lateinit var binding : FragmentHomeBinding
+    private val tabTitles = arrayListOf("talking", "enjoying", "learning", "exercising", "relaxing", "creating", "challenging")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
+        savedInstanceState: Bundle?
     ): View? {
-
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
 
@@ -54,6 +53,10 @@ class HomeFragment : Fragment() {
             it.findNavController().navigate(R.id.action_homeFragment_to_profileFragment)
         }
 
+
+        // 처음 Fragment 그릴 시 적용
+        binding.tabLayout.getTabAt(1)?.view?.setBackgroundResource(R.drawable.tab_unselected2)
+
         return binding.root
     }
 
@@ -61,28 +64,10 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val navController = findNavController()
-
         val appbar = view.findViewById<AppBarLayout>(R.id.appbar)
-        val topbar = view.findViewById<Toolbar>(R.id.topbar)
         val viewPager2 = view.findViewById<ViewPager2>(R.id.viewPager2)
         val tabLayout = view.findViewById<TabLayout>(R.id.tabLayout)
-
-        val activity: AppCompatActivity = getActivity() as AppCompatActivity
-        activity.setSupportActionBar(topbar)
-        activity.supportActionBar?.setTitle("")
-
-
-        appbar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
-
-            when {
-                //  State Expanded
-                verticalOffset == 0 -> Log.d("tob", "확장됨")//  Do anything for Expanded State
-                //  State Collapsed
-                abs(verticalOffset) >= appBarLayout.totalScrollRange -> Log.d("tob",
-                    "축소")//  Do anything for Collapse State
-            }
-        })
+        val tab1 = view.findViewById<TabItem>(R.id.tabtalking1)
 
         viewPager2.adapter =
             PostFragmentStateAdapter(childFragmentManager, viewLifecycleOwner.lifecycle)
@@ -90,24 +75,81 @@ class HomeFragment : Fragment() {
         //스와이프 막기
         viewPager2.isUserInputEnabled = false
 
+
+        // Tablayout에 문자 추가
         TabLayoutMediator(tabLayout, viewPager2) { tab, position ->
-            tab.text = "Tab $position"
+            tab.text = tabTitles[position]
         }.attach()
+//
+////        for (i in 0..7) {
+////            val textView = LayoutInflater.from(requireContext()).inflate(R.layout.tab_title, null) as TextView
+////            binding.tabLayout.getTabAt(i)?.customView = textView
+////        }
+
+        tabLayout.setTabTextColors(Color.WHITE, Color.BLACK)
+
+        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(tab: TabLayout.Tab?) { }
+            override fun onTabUnselected(tab: TabLayout.Tab?) { }
+            @SuppressLint("ResourceAsColor")
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when (tab!!.position) {
+                    0 -> ChangeAni(0, tabLayout)
+                    1 -> {
+                        ChangeAni(1, tabLayout)
+                    }
+                    2 -> {
+                        ChangeAni(2, tabLayout)
+                    }
+                    3 -> {
+                        ChangeAni(3, tabLayout)
+                    }
+                    4 -> {
+                        ChangeAni(4, tabLayout)
+                    }
+                    5 -> {
+                        ChangeAni(5, tabLayout)
+                    }
+                    6 -> {
+                        ChangeAni(6, tabLayout)
+                    }
+                    7 -> {
+                        ChangeAni(7, tabLayout)
+                    }
+                }
+            }
+        })
+
 
         var previous = false
         var collapsed = false
 
         appbar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
-
-
             if (previous != collapsed) {
 //                updateToolbarIconsOnScrollChange(toolbar, collapsed)
                 previous = collapsed
-
             }
         })
-
 //        updateToolbarIconsOnScrollChange(toolbar, collapsed)
+
+    }
+
+    @SuppressLint("ResourceAsColor")
+    public fun ChangeAni(pos : Int, tab : TabLayout){
+        for(i in 0.. 7){
+            tab.getTabAt(i)?.view?.setBackgroundResource(R.drawable.tab_basic)
+        }
+        tab.getTabAt(pos)?.view?.setBackgroundResource(R.drawable.tab_selected_background)
+        if(pos == 0){
+            tab.getTabAt(1)?.view?.setBackgroundResource(R.drawable.tab_unselected2)
+        }
+        else if(pos == 7){
+            tab.getTabAt(6)?.view?.setBackgroundResource(R.drawable.tab_unselected)
+        }
+        else{
+            tab.getTabAt(pos - 1)?.view?.setBackgroundResource(R.drawable.tab_unselected)
+            tab.getTabAt(pos + 1)?.view?.setBackgroundResource(R.drawable.tab_unselected2)
+        }
 
     }
 
