6bd238d0a033e47397fff0989350af1c51369e11
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2022-03-24 02:17:17 +0900

    update OnBoarding
    온보딩 파트 업데이트 했습니다

--- app/src/main/java/com/example/socialproject/mainFragments/FeedFragment.kt
+++ app/src/main/java/com/example/socialproject/mainFragments/FeedFragment.kt
@@ -1,10 +1,12 @@
 package com.example.socialproject.mainFragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatButton
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.example.socialproject.R
@@ -44,4 +46,5 @@ class FeedFragment : Fragment() {
 
         return binding.root
     }
+
 }
