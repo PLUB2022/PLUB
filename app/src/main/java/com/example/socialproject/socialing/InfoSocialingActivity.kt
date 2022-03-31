39e1a019c127695fd9ffbade651eea93e4a07cea
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2022-04-01 02:25:06 +0900

    a

---
+++ app/src/main/java/com/example/socialproject/socialing/InfoSocialingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.socialproject.socialing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InfoSocialingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_socialing)
+    }
+}
