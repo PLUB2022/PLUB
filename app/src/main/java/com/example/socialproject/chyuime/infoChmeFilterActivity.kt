0180d5974a89ccee6f7f509a0df3784db1125544
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2022-04-14 20:51:15 +0900

    기본 틀
    취미모임 틀입니다

---
+++ app/src/main/java/com/example/socialproject/chyuime/infoChmeFilterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.socialproject.chyuime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.socialproject.R
+
+class infoChmeFilterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_chme_filter)
+    }
+}
