340a8d0b28cdeeb47f7aa6d24e21105a6bb991c4
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2022-04-17 02:48:58 +0900

    update login
    네이버 로그인 정보가져오기 까지 구현했습니다.

--- app/src/main/java/com/example/socialproject/login/LoginActivity.kt
+++ app/src/main/java/com/example/socialproject/login/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.socialproject.login
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
@@ -14,6 +15,7 @@ import com.kakao.sdk.common.util.Utility
 import com.nhn.android.naverlogin.OAuthLogin
 import com.nhn.android.naverlogin.OAuthLoginHandler
 import com.nhn.android.naverlogin.ui.view.OAuthLoginButton
+import kotlinx.coroutines.runBlocking
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStream
@@ -21,6 +23,7 @@ import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.MalformedURLException
 import java.net.URL
+import kotlin.concurrent.thread
 
 
 class LoginActivity : AppCompatActivity() {
@@ -54,43 +57,119 @@ class LoginActivity : AppCompatActivity() {
         Log.d("Hash", keyHash)
 
         test_logoutbtn.setOnClickListener {
-            mOAuthLoginInstance.logout(this)
+            Thread{
+                mOAuthLoginInstance.logoutAndDeleteToken(this)
+            }.start()
         }
         loginbtn.setOnClickListener {
             Log.d(
                 "TAG",
                 "Nhn status need login"
             )
-            mOAuthLoginInstance.startOauthLoginActivity(this, @SuppressLint("HandlerLeak") object: OAuthLoginHandler(){
-                override fun run(success: Boolean){
-                    if (success) {
-                        val accessToken = mOAuthLoginInstance.getAccessToken(this@LoginActivity)
-                        val refreshToken = mOAuthLoginInstance.getRefreshToken(this@LoginActivity)
-                        val expiresAt = mOAuthLoginInstance.getExpiresAt(this@LoginActivity)
-                        val tokenType =
-                            mOAuthLoginInstance.getTokenType(this@LoginActivity)
-                        Log.d("TAG", "nhn Login Access Token : $accessToken")
-                        Log.d("TAG", "nhn Login refresh Token : $refreshToken")
-                        Log.d("TAG", "nhn Login expiresAt : $expiresAt")
-                        Log.d("TAG", "nhn Login Token Type : $tokenType")
-                        Log.d("TAG", "nhn Login Module State : "+mOAuthLoginInstance.getState(this@LoginActivity).toString())
-                        val intent = Intent(applicationContext, HomeActivity::class.java)
-                        startActivity(intent)
-                        finish()
-                    } else {
-                        val errorCode =
-                            mOAuthLoginInstance.getLastErrorCode(this@LoginActivity).getCode()
-                        val errorDesc =
-                            mOAuthLoginInstance.getLastErrorDesc(this@LoginActivity)
-                        Log.d("TAG", "errorCode:$errorCode, errorDesc:$errorDesc")
-                        Toast.makeText (this@LoginActivity, "errorCode:$errorCode, errorDesc:$errorDesc", Toast.LENGTH_SHORT).show()
+            mOAuthLoginInstance.startOauthLoginActivity(
+                this,
+                @SuppressLint("HandlerLeak") object : OAuthLoginHandler() {
+                    override fun run(success: Boolean) {
+                        if (success) {
+                            val accessToken = mOAuthLoginInstance.getAccessToken(this@LoginActivity)
+                            val refreshToken =
+                                mOAuthLoginInstance.getRefreshToken(this@LoginActivity)
+                            val expiresAt = mOAuthLoginInstance.getExpiresAt(this@LoginActivity)
+                            val tokenType =
+                                mOAuthLoginInstance.getTokenType(this@LoginActivity)
+                            Log.d("TAG", "nhn Login Access Token : $accessToken")
+                            Log.d("TAG", "nhn Login refresh Token : $refreshToken")
+                            Log.d("TAG", "nhn Login expiresAt : $expiresAt")
+                            Log.d("TAG", "nhn Login Token Type : $tokenType")
+                            Log.d(
+                                "TAG",
+                                "nhn Login Module State : " + mOAuthLoginInstance.getState(this@LoginActivity)
+                                    .toString()
+                            )
+                            val token = "$accessToken" // 네이버 로그인 접근 토큰;
+
+                            val header = "Bearer $token" // Bearer 다음에 공백 추가
+
+
+                            val apiURL = "https://openapi.naver.com/v1/nid/me"
+
+
+                            val requestHeaders: MutableMap<String, String> = HashMap()
+                            requestHeaders["Authorization"] = header
+                            var responseBody = ""
+
+                            val con = connect(apiURL)
+                            try {
+                                Thread{
+                                    con.requestMethod = "GET"
+                                    for ((key, value) in requestHeaders) {
+                                        con.setRequestProperty(key, value)
+                                    }
+
+                                    val responseCode = con.responseCode
+                                    if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 호출
+                                        responseBody = readBody(con.inputStream)
+                                    } else { // 에러 발생
+                                        responseBody = readBody(con.errorStream)
+                                    }
+                                    println(responseBody)
+                                    val intent = Intent(applicationContext, HomeActivity::class.java)
+                                    startActivity(intent)
+                                    finish()
+                                }.start()
+                            } catch (e: IOException) {
+                                throw RuntimeException("API 요청과 응답 실패", e)
+                            } finally {
+                                con.disconnect()
+                            }
+
+
+                        } else {
+                            val errorCode =
+                                mOAuthLoginInstance.getLastErrorCode(this@LoginActivity).getCode()
+                            val errorDesc =
+                                mOAuthLoginInstance.getLastErrorDesc(this@LoginActivity)
+                            Log.d("TAG", "errorCode:$errorCode, errorDesc:$errorDesc")
+                            Toast.makeText(
+                                this@LoginActivity,
+                                "errorCode:$errorCode, errorDesc:$errorDesc",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
                     }
-                }
-            })
+                })
 
         }
 
 
+    }
+
+    private fun connect(apiUrl: String): HttpURLConnection {
+        return try {
+            val url = URL(apiUrl)
+            url.openConnection() as HttpURLConnection
+        } catch (e: MalformedURLException) {
+            throw RuntimeException("API URL이 잘못되었습니다. : $apiUrl", e)
+        } catch (e: IOException) {
+            throw RuntimeException("연결이 실패했습니다. : $apiUrl", e)
+        }
+    }
+
+
+    private fun readBody(body: InputStream): String {
+        val streamReader = InputStreamReader(body)
+        try {
+            BufferedReader(streamReader).use { lineReader ->
+                val responseBody = StringBuilder()
+                var line: String?
+                while (lineReader.readLine().also { line = it } != null) {
+                    responseBody.append(line)
+                }
+                return responseBody.toString()
+            }
+        } catch (e: IOException) {
+            throw RuntimeException("API 응답을 읽는데 실패했습니다.", e)
+        }
     }
 
 }
