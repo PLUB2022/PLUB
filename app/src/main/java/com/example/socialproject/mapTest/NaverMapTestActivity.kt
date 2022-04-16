83aa056f5b969036b6f4e80119b149205a897221
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2022-04-17 05:48:36 +0900

    update now position in map
    현재 위치 지도에 표시되게끔 업데이트 했습니다

--- app/src/main/java/com/example/socialproject/mapTest/NaverMapTestActivity.kt
+++ app/src/main/java/com/example/socialproject/mapTest/NaverMapTestActivity.kt
@@ -1,17 +1,59 @@
 package com.example.socialproject.mapTest
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.FragmentManager
 import com.example.socialproject.R
-import com.naver.maps.map.NaverMapSdk
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.*
 import com.naver.maps.map.NaverMapSdk.NaverCloudPlatformClient
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.util.FusedLocationSource
+import java.util.jar.Manifest
 
 
-class NaverMapTestActivity : AppCompatActivity() {
+class NaverMapTestActivity : AppCompatActivity(), OnMapReadyCallback {
+    private val LOCATION_PERMISSTION_REQUEST_CODE: Int = 1000
+    private lateinit var locationSource: FusedLocationSource // 위치를 반환하는 구현체
+    private lateinit var naverMap: NaverMap
+    private lateinit var mapView: MapView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_naver_map_test)
-
+        mapView = findViewById(R.id.map)
         NaverMapSdk.getInstance(this).client = NaverCloudPlatformClient("as3jcj8e7i")
+
+        mapView.getMapAsync(this)
+        // 위치를 반환하는 구현체인 FusedLocationSource 생성
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSTION_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions,
+                grantResults)) {
+            if (!locationSource.isActivated) { // 권한 거부됨
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            return
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        this.naverMap = naverMap
+        naverMap.locationSource = locationSource
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+        Log.d("TAG", locationSource.lastLocation.toString())
+
+        val cameraPosition = CameraPosition( // 카메라 위치 변경
+            LatLng(37.6281, 127.0905),  // 위치 지정
+            12.3 // 줌 레벨
+        )
+        naverMap.cameraPosition = cameraPosition // 변경된 위치 반영
     }
 }
