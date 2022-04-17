34ab333055e542251c51ec27a64469983b830f70
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2022-04-18 00:12:01 +0900

    Update NaverMapTestActivity.kt
    지도 현재위치 업데이트

--- app/src/main/java/com/example/socialproject/mapTest/NaverMapTestActivity.kt
+++ app/src/main/java/com/example/socialproject/mapTest/NaverMapTestActivity.kt
@@ -1,29 +1,39 @@
 package com.example.socialproject.mapTest
 
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.fragment.app.FragmentManager
 import com.example.socialproject.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.*
 import com.naver.maps.map.NaverMapSdk.NaverCloudPlatformClient
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.util.FusedLocationSource
 import java.util.jar.Manifest
+import kotlin.properties.Delegates
 
 
 class NaverMapTestActivity : AppCompatActivity(), OnMapReadyCallback {
     private val LOCATION_PERMISSTION_REQUEST_CODE: Int = 1000
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var locationSource: FusedLocationSource // 위치를 반환하는 구현체
     private lateinit var naverMap: NaverMap
     private lateinit var mapView: MapView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_naver_map_test)
         mapView = findViewById(R.id.map)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         NaverMapSdk.getInstance(this).client = NaverCloudPlatformClient("as3jcj8e7i")
 
         mapView.getMapAsync(this)
@@ -48,12 +58,44 @@ class NaverMapTestActivity : AppCompatActivity(), OnMapReadyCallback {
         this.naverMap = naverMap
         naverMap.locationSource = locationSource
         naverMap.locationTrackingMode = LocationTrackingMode.Follow
-        Log.d("TAG", locationSource.lastLocation.toString())
 
-        val cameraPosition = CameraPosition( // 카메라 위치 변경
-            LatLng(37.6281, 127.0905),  // 위치 지정
-            12.3 // 줌 레벨
-        )
-        naverMap.cameraPosition = cameraPosition // 변경된 위치 반영
+        var currentLocation: Location?
+        
+        //권한 설정
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                currentLocation = location
+
+                // 카메라 현재위치로 이동
+                val cameraUpdate = CameraUpdate.scrollTo(
+                    LatLng(
+                        currentLocation!!.latitude,
+                        currentLocation!!.longitude
+                    )
+                )
+
+                //현재위치 마커 표시
+                val marker = Marker()
+                marker.position = LatLng( currentLocation!!.latitude, currentLocation!!.longitude)
+                marker.map = naverMap
+                naverMap.moveCamera(cameraUpdate)
+            }
     }
 }
