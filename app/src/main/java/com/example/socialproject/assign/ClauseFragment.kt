53e5d906772a40ba59170b660ce0bb3bf30591d0
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2022-04-21 02:45:14 +0900

    update expand animation
    확장 애니메이션 추가했습니다

--- app/src/main/java/com/example/socialproject/assign/ClauseFragment.kt
+++ app/src/main/java/com/example/socialproject/assign/ClauseFragment.kt
@@ -1,12 +1,16 @@
 package com.example.socialproject.assign
 
+import android.animation.ValueAnimator
 import android.os.Bundle
+import android.os.Handler
 import android.os.PersistableBundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.widget.ViewPager2
@@ -22,9 +26,54 @@ class ClauseFragment(var assignActivity: AssignActivity) : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        var click = false
+        var basicheight : Int = 0
         var next = view.findViewById<AppCompatButton>(R.id.clause_next_btn)
+        var seemorebtn = view.findViewById<AppCompatButton>(R.id.seemoreBtn)
+        var checklayout = view.findViewById<ConstraintLayout>(R.id.checklayout1)
+        var clauseExplainText1 = view.findViewById<TextView>(R.id.clause_explain_text1)
         next.setOnClickListener {
             assignActivity.nextPage()
+        }
+
+
+        seemorebtn.setOnClickListener {
+            if(click){
+                val heightAnimator = ValueAnimator.ofInt(checklayout.height,
+                    (basicheight))
+                heightAnimator.duration = 500
+
+
+                heightAnimator.addUpdateListener { animation: ValueAnimator ->
+                    checklayout.layoutParams.height = animation.animatedValue as Int
+                    checklayout.requestLayout()
+                }
+
+                heightAnimator.start()
+                Handler().postDelayed({
+                    clauseExplainText1.isVisible = false
+                },500)
+                click = false
+            }
+            else{
+                basicheight = checklayout.height
+                val heightAnimator = ValueAnimator.ofInt(checklayout.height,
+                    (checklayout.height * 6)
+                )
+                heightAnimator.duration = 500
+
+
+                heightAnimator.addUpdateListener { animation: ValueAnimator ->
+                    checklayout.layoutParams.height = animation.animatedValue as Int
+                    checklayout.requestLayout()
+                }
+
+                heightAnimator.start()
+                Handler().postDelayed({
+                    clauseExplainText1.isVisible = true
+                },500)
+                click = true
+            }
         }
     }
 
