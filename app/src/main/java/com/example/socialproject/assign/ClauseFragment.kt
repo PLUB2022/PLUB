f63704added846a0097574d4f75cab9df4a9bd84
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2022-04-24 03:04:53 +0900

    update check logic
    체크박스 로직 구현했습니다

--- app/src/main/java/com/example/socialproject/assign/ClauseFragment.kt
+++ app/src/main/java/com/example/socialproject/assign/ClauseFragment.kt
@@ -29,32 +29,82 @@ class ClauseFragment(var assignActivity: AssignActivity) : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         var click = false
-        var basicheight : Int = 0
+        var basicheight: Int = 0
         var next = view.findViewById<AppCompatButton>(R.id.clause_next_btn)
         var seemorebtn = view.findViewById<AppCompatButton>(R.id.seemoreBtn)
         var checklayout = view.findViewById<ConstraintLayout>(R.id.checklayout1)
+        var allcheckBtn = view.findViewById<AppCompatButton>(R.id.allCheckBtn)
+        var allBtnFlag = false
+        var checkBtn1 = view.findViewById<AppCompatButton>(R.id.clauseCheckBtn1)
+        var checkBtn2 = view.findViewById<AppCompatButton>(R.id.clauseCheckBtn2)
+        var checkBtn3 = view.findViewById<AppCompatButton>(R.id.clauseCheckBtn3)
+        var checkBtn4 = view.findViewById<AppCompatButton>(R.id.clauseCheckBtn4)
+        var checkBtn5 = view.findViewById<AppCompatButton>(R.id.clauseCheckBtn5)
+        var checkBtnList =
+            arrayListOf<AppCompatButton>(checkBtn1, checkBtn2, checkBtn3, checkBtn4, checkBtn5)
+        var flagList = arrayListOf<Boolean>(false, false, false, false, false)
         var clauseExplainText1 = view.findViewById<TextView>(R.id.clause_explain_text1)
 
-        var fadeinani : Animation = AnimationUtils.loadAnimation(context, R.anim.fade_in)
-        var fadeoutani : Animation = AnimationUtils.loadAnimation(context, R.anim.fade_out)
+        var fadeinani: Animation = AnimationUtils.loadAnimation(context, R.anim.fade_in)
+        var fadeoutani: Animation = AnimationUtils.loadAnimation(context, R.anim.fade_out)
         val layoutview = view?.findViewById<View>(R.id.clause_assign_layout)
         if (layoutview != null) {
-                layoutview.startAnimation(fadeinani)
+            layoutview.startAnimation(fadeinani)
         }
+
+        for (i in 0..4) {
+            checkBtnList[i].setOnClickListener {
+                if (flagList[i]) {
+                    checkBtnList[i].setBackgroundResource(R.drawable.clause_assign_check_rect)
+                    allcheckBtn.setBackgroundResource(R.drawable.clause_assign_check_rect)
+                    allBtnFlag = false
+                    flagList[i] = false
+                } else {
+                    checkBtnList[i].setBackgroundResource(R.drawable.clause_assign_checked_rect)
+                    flagList[i] = true
+                }
+                if(flagList[0] && flagList[1] && flagList[2] && flagList[3] && flagList[4]){
+                    allcheckBtn.setBackgroundResource(R.drawable.clause_assign_checked_rect)
+                    allBtnFlag = true
+                }
+            }
+        }
+
+        allcheckBtn.setOnClickListener {
+            if (allBtnFlag) {
+                for (i in 0..4) {
+                    checkBtnList[i].setBackgroundResource(R.drawable.clause_assign_check_rect)
+                    flagList[i] = false
+                }
+                allBtnFlag = false
+                allcheckBtn.setBackgroundResource(R.drawable.clause_assign_check_rect)
+            }
+            else{
+                for (i in 0..4) {
+                    checkBtnList[i].setBackgroundResource(R.drawable.clause_assign_checked_rect)
+                    flagList[i] = true
+                }
+                allBtnFlag = true
+                allcheckBtn.setBackgroundResource(R.drawable.clause_assign_checked_rect)
+            }
+        }
+
         next.setOnClickListener {
             if (layoutview != null) {
                 layoutview.startAnimation(fadeoutani)
             }
             Handler().postDelayed({
                 assignActivity.nextPage()
-            },200)
+            }, 200)
         }
 
 
         seemorebtn.setOnClickListener {
-            if(click){
-                val heightAnimator = ValueAnimator.ofInt(checklayout.height,
-                    (basicheight))
+            if (click) {
+                val heightAnimator = ValueAnimator.ofInt(
+                    checklayout.height,
+                    (basicheight)
+                )
                 heightAnimator.duration = 500
 
 
@@ -66,12 +116,12 @@ class ClauseFragment(var assignActivity: AssignActivity) : Fragment() {
                 heightAnimator.start()
                 Handler().postDelayed({
                     clauseExplainText1.isVisible = false
-                },500)
+                }, 500)
                 click = false
-            }
-            else{
+            } else {
                 basicheight = checklayout.height
-                val heightAnimator = ValueAnimator.ofInt(checklayout.height,
+                val heightAnimator = ValueAnimator.ofInt(
+                    checklayout.height,
                     (checklayout.height * 6)
                 )
                 heightAnimator.duration = 500
@@ -85,7 +135,7 @@ class ClauseFragment(var assignActivity: AssignActivity) : Fragment() {
                 heightAnimator.start()
                 Handler().postDelayed({
                     clauseExplainText1.isVisible = true
-                },500)
+                }, 500)
                 click = true
             }
         }
