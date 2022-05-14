9b95cf99cf03c0ff362dea636b9e929175fbfe01
sectionr0 <sectionr0@gmail.com>
2022-05-15 05:24:00 +0900

    Retrofit2 리펙토링
    기존의 RetrofitService 분리 후
    common 폴더에 공용 Retrofit2Client 생성
    
    이후 각 폴더마다 000 Service 를 새로 만들어서 서버와 통신하면 될 것 같습니다.

--- app/src/main/java/com/example/socialproject/common/Retrofit2Client.kt
+++ app/src/main/java/com/example/socialproject/common/Retrofit2Client.kt
@@ -1,30 +1,22 @@
-package com.example.socialproject.login
+package com.example.socialproject.common
 
+import com.example.socialproject.login.LoginService
 import com.google.gson.GsonBuilder
-import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.*
 
-interface RetrofitService {
-
-    @GET("/login/oauth2/naver")
-    fun getNaverLogin(
-        @Header("Authorization") Authorization : String
-    ): Call<String>
-
-
+class Retrofit2Client {
     companion object { // static 처럼 공유객체로 사용가능함. 모든 인스턴스가 공유하는 객체로서 동작함.
         private const val BASE_URL = "http://10.0.2.2:8080/" // 주소
 
-        fun create(): RetrofitService {
+        fun create(): LoginService {
             val gson = GsonBuilder().setLenient().create();
             return Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 //                .client(client)
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .build()
-                .create(RetrofitService::class.java)
+                .create(LoginService::class.java)
         }
     }
 }
